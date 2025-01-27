c86596f5ae7fbeebea4dad9464aedb4687ae49c9
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2025-01-27 11:44:13 +0100

    made it sort of responsive :)

--- client/public/notes.js
+++ client/public/notes.js
@@ -6,6 +6,7 @@ for (var i = 0; i < expandButtons.length; i++) {
     expandButtons[i].addEventListener("click", function(e) {
     
         content.classList.toggle("expanded");
+        content.scrollTop = 0;
         currentButton.style.transform = content.classList.contains("expanded") ? "rotate(180deg)" : "rotate(0deg)";
     }   
     );
