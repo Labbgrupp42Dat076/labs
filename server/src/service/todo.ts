1a249699284ee4b332915abd218c052ee45553f9
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2025-02-04 09:25:45 +0100

    added internal methodology as well as the ability to fetch todos from id

--- server/src/service/todo.ts
+++ server/src/service/todo.ts
@@ -2,8 +2,35 @@ import { TodoObject } from '../model/todoObject';
 
 class todoService {
     todoList: Array<TodoObject> = [];
+    
+
+    // internal methods
+    private  getTodoById(id: number) {
+        let output = this.todoList.find((item) => item.id === id);
+        if (output) {
+            return output;
+        } else {
+            throw new Error('Todo not found');
+        }
+    }
+
+    private updateTodos(todo: TodoObject) {
+        this.todoList = this.todoList.map((item) => {
+            return this.getTodoById(todo.id) === item ? todo : item;
+        });
+
+        return this.todoList;
+    }
+
+
+    // external methods
     public async getTodos() {
         return await this.todoList;
+    }
+
+
+    public async getTodo(id: number){
+        return await this.getTodoById(id)
     }
 
     public async addTodos(todo: TodoObject) {
@@ -11,24 +38,26 @@ class todoService {
         return await this.todoList;
     }
 
-    public async updateTodos(todo: TodoObject) {
-        this.todoList = this.todoList.map((item) => {
-            if (item.id === todo.id) {
-                return todo;
-            }
-            return item;
-        });
-        return await this.todoList;
-    }
+ 
 
     public async deleteTodos(id: number) {
         this.todoList = this.todoList.filter((item) => item.id !== id);
         return await this.todoList;
     }
 
-    public async getTodoById(id: number) {
-        return await this.todoList.find((item) => item.id === id);
+    public async setTodoDone(id: number) {
+        let todo = this.getTodoById(id);
+        todo.completed = true;
+        return await this.updateTodos(todo);
+    }
+
+    public async setTodoUndone(id: number) {
+        let todo = this.getTodoById(id);
+        todo.completed = false;
+        return await this.updateTodos(todo);
     }
 
 
 }
+
+export default new todoService();
