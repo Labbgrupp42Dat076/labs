5438c7101cfcfb2a74ca7aaa1b3dbd85713a543e
Erik <erikpersson0884@gmail.com>
2025-02-11 15:30:50 +0100

    added more tests for todo

--- server/src/test/app.test.ts
+++ server/src/test/app.test.ts
@@ -4,13 +4,29 @@ import { TodoObject } from "../model/todoObject";
 
 const request = SuperTest.default(app);
 
-test("End-to-end test", async () => {
-    const title = "Test title";
-    
-    const res1 = await request.post("/todo").send({title : title});
-    expect(res1.statusCode).toEqual(200);
+const startTodoTitle = "Test title";
 
+beforeAll(async () => {
+    const res1 = await request.post("/todo").send({title : startTodoTitle});
+});
+
+test("Check that the get request works", async () => {
     const res2 = await request.get("/todo");
     expect(res2.statusCode).toEqual(200);
-    expect(res2.body.map((todoObject : TodoObject) => todoObject.title)).toContain(title);
+    expect(res2.body.map((todoObject : TodoObject) => todoObject.title)).toContain(startTodoTitle);
 });
+
+test("Check that todo item has been added after post", async () => {
+    const title = "Test-1";
+
+    const res1 = await request.post("/todo").send({title : title});
+    expect(res1.statusCode).toEqual(200);
+});
+
+test("Check that todo item has been added after wrongful post", async () => {
+    const title = "Test-1";
+
+    const res1 = await request.post("/todo").send({notATitle: title});
+    expect(res1.statusCode).toEqual(404);
+});
+
