db1247790969822d11026b9f780b9a901a946d6c
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2025-02-25 10:50:47 +0100

    added small semantic fixes to tests

--- client/src/test/App.test.tsx
+++ client/src/test/App.test.tsx
@@ -3,14 +3,25 @@ import { TextEncoder } from 'util';
 global.TextEncoder = TextEncoder
 import { render } from '@testing-library/react';
 import App from '../App';
-
+import { StrictMode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 describe('App Component', () => {
 
 
 
     // so you can't really test the routes without mocking the login¨
     test('renders login page', () => {
-        const { getByText } = render(<App />);
+        const { getByText } = render(
+
+            <StrictMode>
+   
+
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </StrictMode>
+        
+        );
         const login = getByText('Login');
         expect(login).toBeInTheDocument();
     });
