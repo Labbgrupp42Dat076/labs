5ba6f26a9d4c85f941f6c797e87acf732cbc079f
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2025-02-25 10:14:32 +0100

    added some packages

--- server/src/start.ts
+++ server/src/start.ts
@@ -9,9 +9,16 @@ import dotenv from 'dotenv';
 
 export const app = express();
 
+dotenv.config();
+if (! process.env.SESSION_SECRET) {
+  
+  console.log("Could not find SESSION_SECRET in .env file");
+  process.exit();
+}
+
 app.use(express.json());
 app.use(session({
-    secret: 'secret', //TODO make this safe or something
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true
 }));
