c528b2a041fa971042126cbda2ba1dfed9dfb395
casper-hansen <60180409+casperHansenTheEnemyOfAustria@users.noreply.github.com>
2025-03-10 10:57:18 +0100

    still tersting

--- server/src/test/fileParser.test.ts
+++ server/src/test/fileParser.test.ts
@@ -5,9 +5,9 @@ import { readTxt } from "../../utilities/fileParser";
 import fs from "fs";
 
 test("readPdf", async () => {
-    // save a sample pdf file in the src/service/uploads folder containitg the text "PDF file"
+    // save a sample pdf file in the src/service/uploads folder containitg the pdf encoded text " PDF file"
 
-    fs.writeFileSync("src/service/uploads/test.pdf", "
+    fs.writeFileSync("src/service/uploads/test.pdf" , "");
     
     const result = await readPdf("test.pdf");
     expect(result).toBe("PDF file");
