81bb72219042d54d91894afbabbd311a6f830c7b
Erik <erikpersson0884@gmail.com>
2025-03-17 21:49:12 +0100

    added swagger documentation to the todo router

--- server/src/router/todo.ts
+++ server/src/router/todo.ts
@@ -11,15 +11,41 @@ import { User } from "../model/user";
 
 const todoService:ITodoService = new TodoDBService();
 
-// get all todos 
+
+/**
+ * @swagger
+ * tags:
+ *   name: Todo
+ *   description: Todo items management and operations
+ */
+
+
+/**
+ * @swagger
+ * /todo:
+ *   get:
+ *     summary: Retrieve all todos for the current user
+ *     tags: [Todo]
+ *     responses:
+ *       200:
+ *         description: A list of todos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/TodoObject'
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 todoRouter.get("/", async (req: Request, res: Response) => {
     try {
-
-        //todo move this to note?? maybe functionally decompose
         const user = await check_session(req);
 
-        const todoIds: number[] = await user.todoIds
-        const todos: TodoObject[]= await todoService.getTodosByListOfIds(todoIds);
+        const todoIds: number[] = user.todoIds;
+        const todos: TodoObject[] = await todoService.getTodosByListOfIds(todoIds);
     
         res.status(200).json(todos);
     } catch (error: unknown) {
@@ -27,7 +53,39 @@ todoRouter.get("/", async (req: Request, res: Response) => {
     }
 });
 
-// get all todos from a list of ids
+
+/**
+ * @swagger
+ * /todo/list:
+ *   get:
+ *     summary: Retrieve todos by a list of IDs
+ *     tags: [Todo]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               ids:
+ *                 type: array
+ *                 items:
+ *                   type: number
+ *                 description: List of todo IDs to retrieve
+ *     responses:
+ *       200:
+ *         description: A list of todos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/TodoObject'
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 todoRouter.get("/list", async (req: Request, res: Response) => {
     try {
         const todos = await todoService.getTodosByListOfIds(req.body.ids);
@@ -37,13 +95,39 @@ todoRouter.get("/list", async (req: Request, res: Response) => {
     }
 });
 
-// delete a todo
+/**
+ * @swagger
+ * /todo/{id}:
+ *   delete:
+ *     summary: Delete a todo by ID
+ *     tags: [Todo]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: ID of the todo to delete
+ *     responses:
+ *       200:
+ *         description: Todo deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
 todoRouter.delete("/:id", async (req: Request, res: Response) => {
     try {
-        const user: User =  await  check_session(req)
+        const user: User = await check_session(req);
 
         user.todoIds = user.todoIds.filter((id: number) => id !== parseInt(req.params.id));
-
 
         req.session.user = user;
 
@@ -54,7 +138,41 @@ todoRouter.delete("/:id", async (req: Request, res: Response) => {
     }
 });
 
-// add a todo
+
+/**
+ * @swagger
+ * /todo:
+ *   post:
+ *     summary: Add a new todo
+ *     tags: [Todo]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: Title of the todo
+ *                 example: "Buy groceries"
+ *     responses:
+ *       200:
+ *         description: Todo added successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 id:
+ *                   type: number
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 todoRouter.post("/", async (req: Request, res: Response) => {
     try {
         const todo: TodoObject = {
@@ -73,7 +191,36 @@ todoRouter.post("/", async (req: Request, res: Response) => {
     }
 });
 
-// done a todo
+
+
+/**
+ * @swagger
+ * /todo/{id}/done:
+ *   post:
+ *     summary: Mark a todo as done
+ *     tags: [Todo]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: ID of the todo to mark as done
+ *     responses:
+ *       200:
+ *         description: Todo marked as done successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 todoRouter.post("/:id/done", async (req: Request, res: Response) => {
     try {
         // check_session(req)
@@ -82,12 +229,38 @@ todoRouter.post("/:id/done", async (req: Request, res: Response) => {
     } catch (error: unknown) {
         ErrorMessage.setResponseToErrorMessage(error, res);
     }
-
 });
 
 
-// undone a todo
 
+/**
+ * @swagger
+ * /todo/{id}/undone:
+ *   post:
+ *     summary: Mark a todo as not done
+ *     tags: [Todo]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: number
+ *         required: true
+ *         description: ID of the todo to mark as not done
+ *     responses:
+ *       200:
+ *         description: Todo marked as not done successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 todoRouter.post("/:id/undone", async (req: Request, res: Response) => {
     try {
         // check_session(req)
@@ -96,7 +269,6 @@ todoRouter.post("/:id/undone", async (req: Request, res: Response) => {
     } catch (error: unknown) {
         ErrorMessage.setResponseToErrorMessage(error, res);
     }
-
 });
 
 
